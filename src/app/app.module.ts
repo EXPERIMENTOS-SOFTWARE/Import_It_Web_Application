fe00fbdf6b372c3e8ad483c19e4f39f857f040db
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2022-09-25 09:22:53 -0500

    added some HTML structure and a validator for the DNI input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import {MatCardModule} from "@angular/material/card";
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatGridListModule} from "@angular/material/grid-list";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+
 
 @NgModule({
   declarations: [
@@ -14,7 +22,16 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.setDNIValidation();
   }
 
+  loginImgSource: string = "https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80";
+
+  submitted: boolean = false;
+
+  loginForm: FormGroup = this.formBuilder.group({
+    // here, we specified the name of all the fields that our form has
+    // in the html file, we define the name of the field in the formControlName attribute
+    dni: ['', { validators: [Validators.required], updateOn:'change'}]
+  })
+
+  get dni() {
+    return this.loginForm.get('dni');
+  }
+
+  setDNIValidation() {
+    const dniControl = this.loginForm.get('dni');
+
+    // validates that the DNI input field only has numbers
+    dniControl?.setValidators([Validators.pattern('^[0-9]*$'), Validators.required]);
+  }
 }
