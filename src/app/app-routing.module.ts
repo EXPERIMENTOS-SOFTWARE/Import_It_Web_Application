fe4352682109cd208d64960fd978978408dfdb75
larson199929 <63831897+larson199929@users.noreply.github.com>
2022-09-28 13:02:21 -0500

    fix(imports): add impor for modules.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ChangePasswordFormComponent } from './change-password-form/change-passw
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/pages/profile/profile.component';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'change', component: ChangePasswordFormComponent },
   { path: 'signUp', component: SignUpFormComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatOptionModule } from '@angular/material/core';
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/pages/profile/profile.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-
 
 @NgModule({
   declarations: [
@@ -36,22 +43,18 @@ import { WelcomeComponent } from './welcome/welcome.component';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule,
+    MatGridListModule,
     MatGridListModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatRadioModule,
     MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    MatToolbarModule,
     MatIconModule,
     MatGridListModule,
     MatSelectModule,
-    MatOptionModule
+    MatOptionModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
