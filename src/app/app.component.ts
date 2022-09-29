3369a1a000f530a0696de53c383345b5aa5f7589
larson199929 <63831897+larson199929@users.noreply.github.com>
2022-09-29 18:43:23 -0500

    feat(landingpage): linked landing page to our web applicattion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
   title = 'ImportItWebApplication';
 
   isSidenavOpen = false;
-
 }
