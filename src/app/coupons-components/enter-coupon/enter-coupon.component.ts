a57635abc147d85449980a13e5bf0e1dda3bc0b7
Edgar <u202020371@upc.edu.pe>
2022-10-18 18:52:07 -0500

    feat(enter-coupon): added component to add coupons

---
+++ src/app/coupons-components/enter-coupon/enter-coupon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-coupon',
+  templateUrl: './enter-coupon.component.html',
+  styleUrls: ['./enter-coupon.component.css']
+})
+export class EnterCouponComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
