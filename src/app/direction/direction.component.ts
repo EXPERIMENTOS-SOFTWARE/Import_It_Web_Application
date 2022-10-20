7bc6ad4670bcbe903117006840de410be9bd247c
larson199929 <63831897+larson199929@users.noreply.github.com>
2022-10-20 03:13:22 -0500

    fix(direction):add cards in direction interface

--- src/app/direction/direction.component.ts
+++ src/app/direction/direction.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Direction } from '../directionregister/direction';
+import { DirectionregisterService } from '../directionregister/directionregister.service';
 
 @Component({
   selector: 'app-direction',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./direction.component.css']
 })
 export class DirectionComponent implements OnInit {
-
-  constructor() { }
+  direction: Direction[] = []
+  constructor(private directionregisterService: DirectionregisterService) { }
 
   ngOnInit(): void {
+    this.getAlldirection();
   }
   directionImgSource: string = "https://i.imgur.com/50r8AVz.png";
+  getAlldirection() {
+    this.directionregisterService.getAll().subscribe((response: any) => {
+      this.direction = response;
+    })
+  }
 }
