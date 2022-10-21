c3830ab25156da97684df5646cbbe72cbd983abd
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2022-10-21 09:29:20 -0500

    feat(comment): add a second comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { UsersComponent } from './users/pages/users/users.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 /*pequeño cambio*/
+/*segundo pequeño cambio*/
 
 @NgModule({
   declarations: [
