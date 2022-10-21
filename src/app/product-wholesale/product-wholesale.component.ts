9778f95cc4ca03dbc11ae9a42718d2eefd447adf
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2022-10-21 14:21:00 -0500

    feat(structure): add structure for product-wholesale

--- src/app/product-wholesale/product-wholesale.component.ts
+++ src/app/product-wholesale/product-wholesale.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
+import { ProductWholesale } from './model/product-wholesale';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-product-wholesale',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductWholesaleComponent implements OnInit {
 
-  constructor() { }
+  numberOfColumns?:number = 3;
+  productsWholesale: ProductWholesale[] = [];
+  sectionNavBar: string = "";
+
+  constructor(private productWholesaleService: ApiService, private route: ActivatedRoute, private router: Router) { 
+
+  }
 
   ngOnInit(): void {
+    this.getAllProductsWholesale();
+    this.numberOfColumns = (window.innerWidth <= 900) ? 1 : 3; 
+    /*this.route.params.pipe( take(1)).subscribe((params) => {
+      this.sectionNavBar = params['type'];
+    });*/
   }
+
+  getAllProductsWholesale(){
+    this.productWholesaleService.getAll().subscribe((response:any)=>{
+      this.productsWholesale = response;
+    })
+  }
+
+  onResize(event:Event) { 
+    this.numberOfColumns = ((<Window>event.target).innerWidth <= 900) ? 1: 3; 
+  }
+
+  
 
 }
